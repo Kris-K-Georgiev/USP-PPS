745942f5b5b63ec1c8ce80ba64010a70eca02573
Kris <krisgeorgiev2002@gmail.bg>
2024-04-30 09:16:45 +0300

    СТР1

--- Back/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const hamburger = document.querySelector('.hamburger');
-  const navbarLinks = document.querySelector('.navbar-links');
-
-  hamburger.addEventListener('click', function() {
-    navbarLinks.classList.toggle('active');
-  });
-});
